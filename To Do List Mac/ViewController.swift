9b58fa52cffbd0084d75b9558c65acb0e3464f68
John <johnkenny1985@gamil.com>
2017-09-05 22:58:06 +0100

    Initial Commit

---
+++ To Do List Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  To Do List Mac
+//
+//  Created by JOHN KENNY on 05/09/2017.
+//  Copyright © 2017 JOHN KENNY. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
